d4c8857bf96ff7c9183cb13a6b6e63a7fb4e9246
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-07-30 05:33:19 -0700

    feat(types): add types

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="node" />
+/**
+ * Returns an "unconventional" conventional-changelog configuration preset.
+ *
+ * `configOverrides`, if an object, is recursively merged into the default
+ * config, overwriting same keys. Exceptions to this include
+ * `writerOpts.generateOn`, `writerOpts.transform`, and
+ * `recommendedBumpOpts.whatBump`. The functions at these keys, if present, will
+ * be invoked _after_ their original implementations; when invoked, they will
+ * receive an additional parameter containing the result from the original
+ * implementation. The only other exception is `types`, which is always merged
+ * via array concatenation. Every other key is deep-merged using
+ * `Object.assign()`.
+ *
+ * If `configOverrides` is a function, it should be of the form
+ * `configOverrides(config) => void`. Ancient-style callbacks of the form
+ * `configOverrides(err, config) => void` are also supported.
+ */
+function main(
+  configOverrides?: (config: Record<string, unknown>) => void
+): typeof import('./defaults');
+function main(
+  configOverrides?: (err: null, config: Record<string, unknown>) => void
+): typeof import('./defaults');
+
+export default main;
